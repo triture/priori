14cc6867734e72265d54aebb8b4460e398043ad0
Felipe Peternella <felipevex@gmail.com>
2016-05-30 02:17:01 -0300

    JQuery plugin adjusts

---
+++ priori/plugins/PriPlugin.hx
@@ -0,0 +1,7 @@
+package priori.plugins;
+
+import jQuery.haxe.Plugin;
+
+extern class PriPlugin implements Plugin {
+    public function datepicker(value:Dynamic = null, arg1:Dynamic = null, arg2:Dynamic = null, arg3:Dynamic = null, arg4:Dynamic = null, arg5:Dynamic = null):Dynamic;
+}
