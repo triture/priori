b438e10d6d863fbc5a7f8a39203c8b409ad86a45
Felipe Peternella <felipevex@gmail.com>
2017-05-14 15:49:39 -0300

    Better rgb color helper

--- priori/geom/PriColor.hx
+++ priori/geom/PriColor.hx
@@ -2,11 +2,11 @@ package priori.geom;
 
 class PriColor {
 
-    private var color:Int;
+    public var color:Int;
 
-    public var red(get, null):Int;
-    public var green(get, null):Int;
-    public var blue(get, null):Int;
+    public var red(get, set):Int;
+    public var green(get, set):Int;
+    public var blue(get, set):Int;
 
     public function new(color:Int = 0x000000) {
         this.color = color;
@@ -16,12 +16,47 @@ class PriColor {
         return (this.color >> 16) & 0xff;
     }
 
+    private function set_red(value:Int):Int {
+        var g:Int = this.green;
+        var b:Int = this.blue;
+
+        this.updateColor(value, g, b);
+
+        return value;
+    }
+
     private function get_green():Int {
         return (this.color >> 8) & 0xff;
     }
 
+    private function set_green(value:Int):Int {
+        var r:Int = this.red;
+        var b:Int = this.blue;
+
+        this.updateColor(r, value, b);
+
+        return value;
+    }
+
     private function get_blue():Int {
         return this.color & 0xff;
+    }
+
+    private function set_blue(value:Int):Int {
+        var r:Int = this.red;
+        var g:Int = this.green;
+
+        this.updateColor(r, g, value);
+
+        return value;
+    }
+
+    public function updateColor(r:Int, g:Int, b:Int):Void {
+        var R:Int = (r << 16);
+        var G:Int = (g << 8);
+        var B:Int = b;
+
+        this.color = R | G | B;
     }
 
     public function toString():String {
