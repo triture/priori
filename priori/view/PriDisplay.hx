8d650fbc97ff5514c4c50496fcf7f521aa12cfcc
Felipe Peternella <felipevex@gmail.com>
2017-08-31 11:18:24 -0300

    bug fix corners

--- priori/view/PriDisplay.hx
+++ priori/view/PriDisplay.hx
@@ -175,12 +175,13 @@ class PriDisplay extends PriEventDispatcher {
     }
 
     private function set_corners(value:Array<Int>):Array<Int> {
-        this.corners = value.copy();
-
         if (value == null || value.length == 0) {
+            this.corners = value == null ? null : [];
             this.dh.jselement.style.borderRadius = "";
         } else {
-                if (value.length > 4) this.dh.jselement.style.borderRadius = value.copy().splice(0, 4).join("px ") + "px";
+            this.corners = value.copy();
+
+            if (value.length > 4) this.dh.jselement.style.borderRadius = value.copy().splice(0, 4).join("px ") + "px";
             this.dh.jselement.style.borderRadius = value.join("px ") + "px";
         }
 
