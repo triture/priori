1048bf69c0ed57154097131d60884fafdb217abb
Felipe Peternella <felipevex@gmail.com>
2023-09-22 13:01:42 -0300

    remove deprecated sintax

--- priori/src-priori/priori/scene/PriSceneManagerEvents.hx
+++ priori/src-priori/priori/scene/PriSceneManagerEvents.hx
@@ -1,6 +1,5 @@
 package priori.scene;
 
-@:enum
-abstract PriSceneManagerEvents(String) from String to String {
+enum abstract PriSceneManagerEvents(String) from String to String {
     var CHANGE_SCENE = 'PRIORI_SCENE_MANAGER_EVENT_CHANGE_SCENE';
 }
